4f3bfd7fdefe486fa33e570013a53f595b721939
Gaurav Singh <gauravsingh13020@gmail.com>
2024-04-24 15:29:37 +0530

    AVL trees added

--- trees/src/com/gaurav/Main.java
+++ trees/src/com/gaurav/Main.java
@@ -10,10 +10,16 @@ public class Main {
 //         trees.populate(scanner);
 //         trees.prettyDisplay();
 
-        BST tree = new BST();
-        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
-        tree.populate(nums);
-        tree.display();
+//        BST tree = new BST();
+//        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+//        tree.populate(nums);
+//        tree.display();
         //hello push
+        TreeAVL tree = new TreeAVL();
+
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
     }
 }
--- trees/src/com/gaurav/TreeAVL.java
+++ trees/src/com/gaurav/TreeAVL.java
@@ -1,6 +1,6 @@
 package com.gaurav;
 
-class BST {
+class TreeAVL {
     public class Node {
         private int value;
         private Node left;
@@ -18,7 +18,7 @@ class BST {
 
     private Node root;
 
-    public BST() {
+    public TreeAVL() {
 
     }
 
@@ -41,7 +41,55 @@ class BST {
         }
 
         node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return rotate(node);
+    }
+
+    private Node rotate(Node node) {
+        //left heavy
+        if (height(node.left) - height(node.right) > 1){
+            if(height(node.left.left)-height(node.left.right) > 0){
+                return rightRotate(node);
+            }
+            if(height(node.left.left)-height(node.left.right) < 0){
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+        // right heavy
+        if (height(node.left) - height(node.right) < -1){
+            if(height(node.right.left)-height(node.right.right) < 0){
+                return leftRotate(node);
+            }
+            if(height(node.left.left)-height(node.left.right) > 0){
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
         return node;
+    }
+
+    private Node leftRotate(Node c){
+        Node p = c.right;
+        Node t = p.left;
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right)+1);
+        c.height = Math.max(height(c.left), height(c.right)+1);
+        return p;
+    }
+
+    private Node rightRotate(Node p){
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right)+1);
+        c.height = Math.max(height(c.left), height(c.right)+1);
+        return c;
     }
 
     public void populate(int[] nums) {
@@ -66,8 +114,9 @@ class BST {
         populatedSorted(nums, mid + 1, end);
     }
 
-    public void display() {
+    public boolean display() {
         display(this.root, "Root Node: ");
+        return false;
     }
 
     private void display(Node node, String details) {
@@ -104,3 +153,4 @@ class BST {
     }
 
 }
+
